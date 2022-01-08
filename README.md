610bc91c23068ae9f098c99079a43f38e744bf1e
your name <your mail@technikum-wien.at>
2022-01-08 17:23:59 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# MTCG
+Monster Trading Cards Game (MTCG) __ This HTTP/REST-based server is built to be a platform for trading and battling with and  against each other in a magical card-game world. 
---
+++ src/main/java/Main.java
@@ -0,0 +1,16 @@
+//package
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //Thread starten für den server zum hören im hintergrund, könnte auch eienn 2. rest server starten.
+
+        int x = 2;
+        System.out.println(x);
+
+
+
+    }
+
+}
